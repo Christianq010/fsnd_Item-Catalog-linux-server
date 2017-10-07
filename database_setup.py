96d6bbcf500f8e1d7098391a7aa40bea2219990f
Christiaan Quyn <christianq010@gmail.com>
2017-10-07 11:25:56 +0530

    Feat: Successfully add Database file by creating setup with classes and running it

---
+++ database_setup.py
@@ -0,0 +1,68 @@
+# Add User table to store User information in our Database for Local permission system
+
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    item_list = relationship("Item", cascade="all, delete-orphan")
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'catalog_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(500))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+    # Add our decorator method for out JSON response
+    @property
+    # Returns object data in easily serializable format
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id
+        }
+
+
+# Insert at end of file
+engine = create_engine('sqlite:///catalogitems.db')
+
+Base.metadata.create_all(engine)
+
+
